6afb21947d14fb0db9ebc5a7b7728ac1cec19a63
pubudu2003060 <madhushanpubudu2@gmail.com>
2026-03-19 22:04:00 +0530

    feat:add dockercompose for databse and .envsetup for getdata

---
+++ backend/taskmanager-backend/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS task (
+    id UUID PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50),
+    created_at TIMESTAMP
+);
