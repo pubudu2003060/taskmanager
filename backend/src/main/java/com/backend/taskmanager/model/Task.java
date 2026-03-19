38ff8ed99a19bebfe865951c901096eb3941e9cb
pubudu2003060 <madhushanpubudu2@gmail.com>
2026-03-19 22:28:36 +0530

    application succesfully connec with the database

---
+++ backend/src/main/java/com/backend/taskmanager/model/Task.java
@@ -0,0 +1,32 @@
+package com.backend.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+}
